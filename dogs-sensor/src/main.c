f43db51c50f58b99efad42195ec1ee1637151e48
Jan Olencki <jan@olencki.pl>
2021-04-09 18:15:22 +0200

    Work in progress

--- dogs-sensor/src/main.c
+++ dogs-sensor/src/main.c
@@ -1,84 +1,108 @@
-/* WiFi softAP Example
-
-   This example code is in the Public Domain (or CC0 licensed, at your option.)
-
-   Unless required by applicable law or agreed to in writing, this
-   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-   CONDITIONS OF ANY KIND, either express or implied.
+/**
+ * @file example.c
+ * 
+ * @author
+ * Angelo Elias Dalzotto (150633@upf.br)
+ * GEPID - Grupo de Pesquisa em Cultura Digital (http://gepid.upf.br/)
+ * Universidade de Passo Fundo (http://www.upf.br/)
+ * 
+ * @copyright
+ * Copyright (c) 2017 Raivis Strogonovs (https://morf.lv)
+ * 
+ * @brief This is an example file to the MAX30100 library for the ESP32.
+ * It initializes the IDF-SDK I2C driver and then initializes the sensor.
+ * It crates a task to update the readings at a rate of 100Hz and prints
+ * the bpm and oxigen saturation results.
 */
-#include <string.h>
-#include "freertos/FreeRTOS.h"
+/**
+ * Pin assignment:
+ * - i2c:
+ *    GPIO12: SDA
+ *    GPIO14: SDL
+ * - no need to add external pull-up resistors.
+ */
+
+#include <stdio.h>
+#include "driver/i2c.h"
 #include "freertos/task.h"
-#include "esp_system.h"
-#include "esp_wifi.h"
-#include "esp_event.h"
-#include "esp_log.h"
-#include "nvs_flash.h"
+#include "driver/gpio.h"
 
-#include "lwip/err.h"
-#include "lwip/sys.h"
+#include "max30100/max30100.h"
 
-#define EXAMPLE_ESP_WIFI_SSID      "mywifissid"
-#define EXAMPLE_ESP_WIFI_PASS      "mywifipass"
-#define EXAMPLE_MAX_STA_CONN       (3)
+#define I2C_SDA 12
+#define I2C_SCL 14
+#define I2C_FRQ 100000
+#define I2C_PORT I2C_NUM_0
 
-static const char *TAG = "wifi softAP";
+max30100_config_t max30100 = {};
 
-static void wifi_event_handler(void* arg, esp_event_base_t event_base,
-                                    int32_t event_id, void* event_data)
-{
-    if (event_id == WIFI_EVENT_AP_STACONNECTED) {
-        wifi_event_ap_staconnected_t* event = (wifi_event_ap_staconnected_t*) event_data;
-        ESP_LOGI(TAG, "station "MACSTR" join, AID=%d",
-                 MAC2STR(event->mac), event->aid);
-    } else if (event_id == WIFI_EVENT_AP_STADISCONNECTED) {
-        wifi_event_ap_stadisconnected_t* event = (wifi_event_ap_stadisconnected_t*) event_data;
-        ESP_LOGI(TAG, "station "MACSTR" leave, AID=%d",
-                 MAC2STR(event->mac), event->aid);
-    }
+esp_err_t i2c_master_init(i2c_port_t i2c_port){
+    i2c_config_t conf = {};
+    conf.mode = I2C_MODE_MASTER;
+    conf.sda_io_num = I2C_SDA;
+    conf.scl_io_num = I2C_SCL;
+    conf.sda_pullup_en = GPIO_PULLUP_ENABLE;
+    conf.scl_pullup_en = GPIO_PULLUP_ENABLE;
+    conf.master.clk_speed = I2C_FRQ;
+    i2c_param_config(i2c_port, &conf);
+    return i2c_driver_install(i2c_port, I2C_MODE_MASTER, 0, 0, 0);
 }
 
-void wifi_init_softap()
-{
-    tcpip_adapter_init();
-    ESP_ERROR_CHECK(esp_event_loop_create_default());
-
-    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
-    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
-
-    ESP_ERROR_CHECK(esp_event_handler_register(WIFI_EVENT, ESP_EVENT_ANY_ID, &wifi_event_handler, NULL));
-
-    wifi_config_t wifi_config = {
-        .ap = {
-            .ssid = EXAMPLE_ESP_WIFI_SSID,
-            .ssid_len = strlen(EXAMPLE_ESP_WIFI_SSID),
-            .password = EXAMPLE_ESP_WIFI_PASS,
-            .max_connection = EXAMPLE_MAX_STA_CONN,
-            .authmode = WIFI_AUTH_WPA_WPA2_PSK
-        },
-    };
-    if (strlen(EXAMPLE_ESP_WIFI_PASS) == 0) {
-        wifi_config.ap.authmode = WIFI_AUTH_OPEN;
+void get_bpm(void* param) {
+    printf("MAX30100 Test\n");
+    max30100_data_t result = {};
+    while(true) {
+        //Update sensor, saving to "result"
+        ESP_ERROR_CHECK(max30100_update(&max30100, &result));
+        if(result.pulse_detected) {
+            printf("BEAT\n");
+            printf("BPM: %f | SpO2: %f%%\n", result.heart_bpm, result.spO2);
+        }
+        //Update rate: 100Hz
+        vTaskDelay(100/portTICK_PERIOD_MS);
+        printf("Print regs\n");
+        max30100_print_registers(&max30100);
     }
-
-    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_AP));
-    ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_AP, &wifi_config));
-    ESP_ERROR_CHECK(esp_wifi_start());
-
-    ESP_LOGI(TAG, "wifi_init_softap finished. SSID:%s password:%s",
-             EXAMPLE_ESP_WIFI_SSID, EXAMPLE_ESP_WIFI_PASS);
 }
 
 void app_main()
 {
-    //Initialize NVS
-    esp_err_t ret = nvs_flash_init();
-    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
-      ESP_ERROR_CHECK(nvs_flash_erase());
-      ret = nvs_flash_init();
-    }
-    ESP_ERROR_CHECK(ret);
-
-    ESP_LOGI(TAG, "ESP_WIFI_MODE_AP");
-    wifi_init_softap();
+    //Init I2C_NUM_0
+    ESP_ERROR_CHECK(i2c_master_init(I2C_PORT));
+    //Init sensor at I2C_NUM_0
+    ESP_ERROR_CHECK(max30100_init( &max30100, I2C_PORT,
+                   MAX30100_DEFAULT_OPERATING_MODE,
+                   MAX30100_DEFAULT_SAMPLING_RATE,
+                   MAX30100_DEFAULT_LED_PULSE_WIDTH,
+                   MAX30100_DEFAULT_IR_LED_CURRENT,
+                   MAX30100_DEFAULT_START_RED_LED_CURRENT,
+                   MAX30100_DEFAULT_MEAN_FILTER_SIZE,
+                   MAX30100_DEFAULT_PULSE_BPM_SAMPLE_SIZE,
+                   true, false ));
+    printf("Print regs\n");
+    max30100_print_registers(&max30100);
+    //Start test task
+    xTaskCreate(get_bpm, "Get BPM", 8192, NULL, 1, NULL);
 }
+
+/**
+ * MIT License
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+*/
