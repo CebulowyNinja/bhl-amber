ef9e7790d90008a0fd02e795d36dcd217d471818
Jan Olencki <jan@olencki.pl>
2021-04-10 09:11:22 +0200

    Acellerotemeter work in progress

--- dogs-sensor/src/main.cpp
+++ dogs-sensor/src/main.cpp
@@ -1,5 +1,5 @@
 #include <Arduino.h>
-  
+
 /*
   Optical Heart Rate Detection (PBA Algorithm) using the MAX30105 Breakout
   By: Nathan Seidle @ SparkFun Electronics
@@ -20,53 +20,84 @@
   The MAX30105 Breakout can handle 5V or 3.3V I2C logic. We recommend powering the board with 5V
   but it will also run at 3.3V.
 */
+#include "I2Cdev.h"
+#include "MPU6050_6Axis_MotionApps20.h"
 
 #include <Wire.h>
 #include "MAX30105.h"
 
 #include "heartRate.h"
-
+#define INTERRUPT_PIN 18
+// HR
 MAX30105 particleSensor;
-
 const byte RATE_SIZE = 4; //Increase this for more averaging. 4 is good.
-byte rates[RATE_SIZE]; //Array of heart rates
+byte rates[RATE_SIZE];    //Array of heart rates
 byte rateSpot = 0;
 long lastBeat = 0; //Time at which the last beat occurred
 
 float beatsPerMinute;
 int beatAvg;
 
-typedef struct _hr_rate_t {
+typedef struct _hr_rate_t
+{
   long ir;
   float bpm;
   int bpm_avg;
   bool is_valid;
 } hr_rate_t;
 
+// ACC
+MPU6050 mpu;
+bool dmpReady = false;  // set true if DMP init was successful
+uint8_t mpuIntStatus;   // holds actual interrupt status byte from MPU
+uint8_t devStatus;      // return status after each device operation (0 = success, !0 = error)
+uint16_t packetSize;    // expected DMP packet size (default is 42 bytes)
+uint16_t fifoCount;     // count of all bytes currently in FIFO
+uint8_t fifoBuffer[64]; // FIFO storage buffer
+
+Quaternion q;                       // [w, x, y, z]         quaternion container
+VectorInt16 aa;                     // [x, y, z]            accel sensor measurements
+VectorInt16 aaReal;                 // [x, y, z]            gravity-free accel sensor measurements
+VectorInt16 aaWorld;                // [x, y, z]            world-frame accel sensor measurements
+VectorFloat gravity;                // [x, y, z]            gravity vector
+float euler[3];                     // [psi, theta, phi]    Euler angle container
+float ypr[3];                       // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
+volatile bool mpuInterrupt = false; // indicates whether MPU interrupt pin has gone high
+void dmpDataReady()
+{
+  mpuInterrupt = true;
+}
+
 void hr_rate_init();
+void acc_init();
 void setup()
 {
   Serial.begin(115200);
   Serial.println("Initializing...");
   hr_rate_init();
+  acc_init();
 }
 
 hr_rate_t hr_rate_meas();
+void acc_loop();
 void loop()
 {
   hr_rate_t hr_rate = hr_rate_meas();
-  Serial.print("IR=");
-  Serial.print(hr_rate.ir);
-  Serial.print(", BPM=");
-  Serial.print(hr_rate.bpm);
-  Serial.print(", BPM_AVG=");
-  Serial.print(hr_rate.bpm_avg);
-  Serial.print(", IS_VALID=");
-  Serial.print(hr_rate.is_valid);
-  Serial.println();
+  // Serial.print("IR=");
+  // Serial.print(hr_rate.ir);
+  // Serial.print(", BPM=");
+  // Serial.print(hr_rate.bpm);
+  // Serial.print(", BPM_AVG=");
+  // Serial.print(hr_rate.bpm_avg);
+  // Serial.print(", IS_VALID=");
+  // Serial.print(hr_rate.is_valid);
+  // Serial.println();
+
+  acc_loop();
 }
 
-void hr_rate_init() {
+void hr_rate_init()
+{
   // Initialize sensor
   if (!particleSensor.begin(Wire, I2C_SPEED_FAST)) //Use default I2C port, 400kHz speed
   {
@@ -74,12 +105,13 @@ void hr_rate_init() {
   }
   Serial.println("Place your index finger on the sensor with steady pressure.");
 
-  particleSensor.setup(); //Configure sensor with default settings
+  particleSensor.setup();                    //Configure sensor with default settings
   particleSensor.setPulseAmplitudeRed(0x0A); //Turn Red LED to low to indicate sensor is running
-  particleSensor.setPulseAmplitudeGreen(0); //Turn off Green LED
+  particleSensor.setPulseAmplitudeGreen(0);  //Turn off Green LED
 }
 
-hr_rate_t hr_rate_meas() {
+hr_rate_t hr_rate_meas()
+{
   hr_rate_t hr;
   long irValue = particleSensor.getIR();
 
@@ -94,11 +126,11 @@ hr_rate_t hr_rate_meas() {
     if (beatsPerMinute < 255 && beatsPerMinute > 20)
     {
       rates[rateSpot++] = (byte)beatsPerMinute; //Store this reading in the array
-      rateSpot %= RATE_SIZE; //Wrap variable
+      rateSpot %= RATE_SIZE;                    //Wrap variable
 
       //Take average of readings
       beatAvg = 0;
-      for (byte x = 0 ; x < RATE_SIZE ; x++)
+      for (byte x = 0; x < RATE_SIZE; x++)
         beatAvg += rates[x];
       beatAvg /= RATE_SIZE;
     }
@@ -111,4 +143,167 @@ hr_rate_t hr_rate_meas() {
     hr.is_valid = false;
 
   return hr;
+}
+
+void acc_init()
+{
+  mpu.initialize();
+  pinMode(INTERRUPT_PIN, INPUT);
+
+  // verify connection
+  Serial.println(F("Testing device connections..."));
+  Serial.println(mpu.testConnection() ? F("MPU6050 connection successful") : F("MPU6050 connection failed"));
+  devStatus = mpu.dmpInitialize();
+
+  // supply your own gyro offsets here, scaled for min sensitivity
+  mpu.setXGyroOffset(63);
+  mpu.setYGyroOffset(14);
+  mpu.setZGyroOffset(-39);
+  mpu.setZAccelOffset(1788); // 1688 factory default for my test chip
+
+  // make sure it worked (returns 0 if so)
+  if (devStatus == 0)
+  {
+    // Calibration Time: generate offsets and calibrate our MPU6050
+    mpu.CalibrateAccel(6);
+    mpu.CalibrateGyro(6);
+    mpu.PrintActiveOffsets();
+    // turn on the DMP, now that it's ready
+    Serial.println(F("Enabling DMP..."));
+    mpu.setDMPEnabled(true);
+
+    // enable Arduino interrupt detection
+    Serial.print(F("Enabling interrupt detection (Arduino external interrupt "));
+    Serial.print(digitalPinToInterrupt(INTERRUPT_PIN));
+    Serial.println(F(")..."));
+    attachInterrupt(digitalPinToInterrupt(INTERRUPT_PIN), dmpDataReady, RISING);
+    mpuIntStatus = mpu.getIntStatus();
+
+    // set our DMP Ready flag so the main loop() function knows it's okay to use it
+    Serial.println(F("DMP ready! Waiting for first interrupt..."));
+    dmpReady = true;
+
+    // get expected DMP packet size for later comparison
+    packetSize = mpu.dmpGetFIFOPacketSize();
+  }
+  else
+  {
+    // ERROR!
+    // 1 = initial memory load failed
+    // 2 = DMP configuration updates failed
+    // (if it's going to break, usually the code will be 1)
+    Serial.print(F("DMP Initialization failed (code "));
+    Serial.print(devStatus);
+    Serial.println(F(")"));
+  }
+}
+
+void acc_loop()
+{
+  // if programming failed, don't try to do anything
+  if (!dmpReady)
+    return;
+
+  // wait for MPU interrupt or extra packet(s) available
+  while (!mpuInterrupt && fifoCount < packetSize)
+  {
+    if (mpuInterrupt && fifoCount < packetSize)
+    {
+      // try to get out of the infinite loop
+      fifoCount = mpu.getFIFOCount();
+    }
+    // other program behavior stuff here
+    // .
+    // .
+    // .
+    // if you are really paranoid you can frequently test in between other
+    // stuff to see if mpuInterrupt is true, and if so, "break;" from the
+    // while() loop to immediately process the MPU data
+    // .
+    // .
+    // .
+  }
+
+  // reset interrupt flag and get INT_STATUS byte
+  mpuInterrupt = false;
+  mpuIntStatus = mpu.getIntStatus();
+
+  // get current FIFO count
+  fifoCount = mpu.getFIFOCount();
+  if (fifoCount < packetSize)
+  {
+    //Lets go back and wait for another interrupt. We shouldn't be here, we got an interrupt from another event
+    // This is blocking so don't do it   while (fifoCount < packetSize) fifoCount = mpu.getFIFOCount();
+  }
+  // check for overflow (this should never happen unless our code is too inefficient)
+  else if ((mpuIntStatus & (0x01 << MPU6050_INTERRUPT_FIFO_OFLOW_BIT)) || fifoCount >= 1024)
+  {
+    // reset so we can continue cleanly
+    mpu.resetFIFO();
+    //  fifoCount = mpu.getFIFOCount();  // will be zero after reset no need to ask
+    Serial.println(F("FIFO overflow!"));
+
+    // otherwise, check for DMP data ready interrupt (this should happen frequently)
+  }
+  else if (mpuIntStatus & (0x01 << MPU6050_INTERRUPT_DMP_INT_BIT))
+  {
+
+    // read a packet from FIFO
+    while (fifoCount >= packetSize)
+    { // Lets catch up to NOW, someone is using the dreaded delay()!
+      mpu.getFIFOBytes(fifoBuffer, packetSize);
+      // track FIFO count here in case there is > 1 packet available
+      // (this lets us immediately read more without waiting for an interrupt)
+      fifoCount -= packetSize;
+    }
+    accelgyro.getAcceleration(&ax, &ay, &az);
+
+    // display Euler angles in degrees
+    // mpu.dmpGetQuaternion(&q, fifoBuffer);
+    // mpu.dmpGetEuler(euler, &q);
+    // Serial.print("euler\t");
+    // Serial.print(euler[0] * 180 / M_PI);
+    // Serial.print("\t");
+    // Serial.print(euler[1] * 180 / M_PI);
+    // Serial.print("\t");
+    // Serial.println(euler[2] * 180 / M_PI);
+
+    // display Euler angles in degrees
+    // mpu.dmpGetQuaternion(&q, fifoBuffer);
+    // mpu.dmpGetGravity(&gravity, &q);
+    // mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
+    // Serial.print("ypr\t");
+    // Serial.print(ypr[0] * 180 / M_PI);
+    // Serial.print("\t");
+    // Serial.print(ypr[1] * 180 / M_PI);
+    // Serial.print("\t");
+    // Serial.println(ypr[2] * 180 / M_PI);
+
+    // display real acceleration, adjusted to remove gravity
+    // mpu.dmpGetQuaternion(&q, fifoBuffer);
+    // mpu.dmpGetAccel(&aa, fifoBuffer);
+    // mpu.dmpGetGravity(&gravity, &q);
+    // mpu.dmpGetLinearAccel(&aaReal, &aa, &gravity);
+    // Serial.print("areal\t");
+    // Serial.print(aaReal.x);
+    // Serial.print("\t");
+    // Serial.print(aaReal.y);
+    // Serial.print("\t");
+    // Serial.println(aaReal.z);
+
+    // display initial world-frame acceleration, adjusted to remove gravity
+    // and rotated based on known orientation from quaternion
+    // mpu.dmpGetQuaternion(&q, fifoBuffer);
+    // mpu.dmpGetAccel(&aa, fifoBuffer);
+    // mpu.dmpGetGravity(&gravity, &q);
+    // mpu.dmpGetLinearAccel(&aaReal, &aa, &gravity);
+    // mpu.dmpGetLinearAccelInWorld(&aaWorld, &aaReal, &q);
+    // Serial.print("aworld\t");
+    // Serial.print(aaWorld.x);
+    // Serial.print("\t");
+    // Serial.print(aaWorld.y);
+    // Serial.print("\t");
+    // Serial.println(aaWorld.z);
+
+  }
 }
